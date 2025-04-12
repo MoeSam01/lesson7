d7ba160847e6a4c125cee93268af4024d1c826d6
MoeSam01 <91637744+MoeSam01@users.noreply.github.com>
2025-04-11 20:58:45 -0400

    description

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+function fetchGradeData() {
+    console.log("fetchGradeData function called");
+  }
+
+  function populateGradebook(data) {
+    console.log("populateGradebook function called with data:", data);
+  }
+  
+  fetchGradeData();
+  populateGradebook([]);
+  
